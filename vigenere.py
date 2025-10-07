de5bd7ce3a3a8c1f04e32581fbb4aee0ef8e7aaf
David <david@Davids-MacBook-Air.local>
2025-10-07 21:46:17 +0200

    feat: implementes vigenere encryption. Decryption remains to be implemented

--- vigenere.py
+++ vigenere.py
@@ -1,12 +1,17 @@
 #Vigenere uses a vector of k length to determine period length and also how many keys to shift.
 #It is like a substitution cipher that changes every char within a specific period.
 
+def get_nonempty_input(prompt):
+    while True:
+        value = input(prompt)
+        if value:
+            return value
+        print("Input cannot be empty, try again.")
+
 def vigenereEncrypt():
     print("you are encrypting using vigenere cipher")
-    plaintext = list(input("write the desired plaintext: "))
-    plaintextAscii = [ord(c) for c in plaintext]
-    key = list(input("write the key: "))
-    #asciiKey = [ord(c) for c in key]
+    plaintext = list(get_nonempty_input("write the desired plaintext: "))
+    key = list(get_nonempty_input("write the key: "))
     keyShift = []
     ciphertext = []
     for char in key:
@@ -14,11 +19,16 @@ def vigenereEncrypt():
             keyShift.append(ord(char)-97)
         elif char.isupper():
             keyShift.append(ord(char)-65)
-    for i in plaintextAscii:
-#            shift = (ord(char) - 97 + key) % 26 + 97
-            ciphertext.append(plaintextAscii[i]+keyShift[i])
-
-    print (ciphertext)
+    for i, char in enumerate(plaintext):
+        if char.islower():
+            shift = (ord(char) - 97 + keyShift[i % len(keyShift)]) % 26 + 97
+            ciphertext.append(chr(shift))
+        elif char.isupper():
+            shift = (ord(char) - 65 + keyShift[i % len(keyShift)]) % 26 + 65
+            ciphertext.append(chr(shift))
+        else:
+            ciphertext.append(char)
+    print("".join(ciphertext))
         
 
 
